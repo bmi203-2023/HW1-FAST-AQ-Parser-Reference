a47fef033a821c6c7f3026232e4b7693e39077e5
Alex Lee <alex.j.lee03@gmail.com>
2023-01-11 19:16:27 -0800

    initial commit

---
+++ tests/test_parsers.py
@@ -0,0 +1,71 @@
+# write tests for parsers
+
+from seqparser import (
+    FastaParser,
+    FastqParser,
+)
+
+import pathlib
+import pytest
+
+
+def test_freebie_parser_1():
+    """
+    This one is a freebie
+    DO NOT MODIFY THIS FUNCTION
+    """
+    assert True
+
+
+def test_freebie_parser_2():
+    """
+    This too is a freebie
+    DO NOT MODIFY THIS FUNCTION
+    """
+    assert 1 != 2
+
+
+def get_filepath(which):
+    data_dir = pathlib.Path(__file__).resolve().parent.parent / "data"
+    if which == "fasta":
+        return data_dir / "test.fa"
+    else:
+        return data_dir / "test.fq"
+
+
+def open_fastq_reference():
+    f = pathlib.Path(__file__).resolve().parent / "fastq-check.txt"
+    with f.open() as f:
+        seqs = list(map(lambda l: l.strip().split("|"), f.readlines()))
+    return seqs  # will be list of lists with seq, quality that were parsed from the test files using get-seq.sh
+
+
+def open_fasta_reference():
+    f = pathlib.Path(__file__).resolve().parent / "fasta-check.txt"
+    with f.open() as f:
+        seqs = list(map(lambda l: l.strip(), f.readlines()))
+    return seqs  # will be a list of seqs, quality that were parsed from the test files using get-seq.sh
+
+
+def test_FastaParser():
+    """
+    Write your unit test for your FastaParser
+    class here. You should generate an instance of
+    your FastaParser class and assert that it properly
+    reads in the example Fasta File. You could also
+    write a test that checks that the parser 
+    does not work when given incorrect input.
+    """
+    assert True
+
+def test_FastqParser():
+    """
+    Write your unit test for your FastqParser
+    class here. You should generate an instance of
+    your FastqParser class and assert that it properly
+    reads in the example Fastq File. You could also 
+    write a test that checks that the parser 
+    does not work when given incorrect input.
+    """
+    assert True
+
---
+++ tests/test_transcribe.py
@@ -0,0 +1,36 @@
+# write tests for transcribes
+import pytest
+from seqparser import transcribe, reverse_transcribe
+
+SEQ = "ACTGAACCC"
+
+
+def test_freebie_transcribe_1():
+    """
+    This one is a freebie
+    DO NOT MODIFY THIS FUNCTION
+    """
+    assert True
+
+
+def test_freebie_transcribe_2():
+    """
+    This too is a freebie
+    DO NOT MODIFY THIS FUNCTION
+    """
+    assert 1 != 2
+
+
+def test_transcribe():
+    """
+    Write your unit test for the
+    transcribe function here.
+    """
+    assert True
+
+def test_reverse_transcribe():
+    """
+    Write your unit test for the
+    reverse transcribe function here.
+    """
+    assert True
